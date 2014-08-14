c0beee21a51e8682cff087a0c9f5c5496fc1a167
Sam <notmye@gmail.com>
2014-08-14 09:55:58 +0100

    Fixed hubs and added comments

--- Physics fighter/Physics fighter/Hubs/GameHub.cs
+++ Physics fighter/Physics fighter/Hubs/GameHub.cs
@@ -4,13 +4,18 @@ using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
 
-namespace Physics_fighter
+namespace Physics_fighter.Hubs
 {
     public class GameHub : Hub
     {
+        //This updates the physic frames on all of the clients
         public void SetObjectFrame(int id,int frame, float posx, float posy,string colour)
         {
             Clients.All.SetObjectFrame(id,frame,posx,posy,colour);
+        }
+        //this function is called by the clients and applys the forces to the user bodys allong with readying them
+        public void ReadyState()
+        { 
         }
     }
 }
